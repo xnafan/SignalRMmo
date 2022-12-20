0a33f0d93bc5cc29cca799a915fa782a9714b700
Jakob Farian Krarup <kr@rup.dk>
2022-12-20 20:20:54 +0100

    Cleaning architecture

---
+++ SignalRMmo/wwwroot/js/GameCanvas.js
@@ -0,0 +1,54 @@
+﻿class GameCanvas {
+
+    #keyboardStatus = { up: false, down: false, left: false, right: false };
+    #ctx;
+    #canvas;
+    constructor(canvas) {
+        this.#canvas = canvas;
+        this.#ctx = canvas.getContext("2d");
+        canvas.focus();
+        canvas.addEventListener("keydown", (event) => this.#keyDown(event));
+        canvas.addEventListener("keyup", (event) => this.#keyUp(event));
+        canvas.addEventListener("blur", (event) => this.#allKeysUp(event));
+    }
+
+    #allKeysUp() {
+        this.#keyboardStatus.down = false;
+        this.#keyboardStatus.up = false;
+        this.#keyboardStatus.left = false;
+        this.#keyboardStatus.right = false;
+    }
+    #keyDown(event) {
+        switch (event.code) {
+            case 'ArrowLeft': this.#keyboardStatus.left = true; break;
+            case 'ArrowRight': this.#keyboardStatus.right = true; break;
+            case 'ArrowUp': this.#keyboardStatus.up = true; break;
+            case 'ArrowDown': this.#keyboardStatus.down = true; break;
+        }
+    }
+
+    #keyUp(event) {
+    switch (event.code) {
+        case 'ArrowLeft': this.#keyboardStatus.left = false; break;
+        case 'ArrowRight': this.#keyboardStatus.right = false; break;
+        case 'ArrowUp': this.#keyboardStatus.up = false; break;
+        case 'ArrowDown': this.#keyboardStatus.down = false; break;
+    }
+}
+
+    get keyboardState() { return this.#keyboardStatus; }
+
+    get context() { return this.#ctx; }
+    get width() { return this.#canvas.width; }
+    get height() { return this.#canvas.height; }
+
+    getMovement() {
+        var deltaX = 0;
+        deltaX += this.#keyboardStatus.left ? -1 : 0;
+        deltaX += this.#keyboardStatus.right ? 1 : 0;
+        var deltaY = 0;
+        deltaY += this.#keyboardStatus.up ? -1 : 0;
+        deltaY += this.#keyboardStatus.down ? 1 : 0;
+        return { x: deltaX, y: deltaY };
+    }
+}
--- SignalRMmo/wwwroot/js/mmo.js
+++ SignalRMmo/wwwroot/js/mmo.js
@@ -1,17 +1,19 @@
 ﻿"use strict";
-let gameCanvas = document.getElementById("gameCanvas");
-const ctx = gameCanvas.getContext("2d");
-gameCanvas.addEventListener("keydown", keyDown);
-gameCanvas.addEventListener("keyup", keyUp);
+const gameCanvas = new GameCanvas(document.getElementById("gameCanvas"));
 
 let playerId = "";
-let speed = 5;
+const speed = 5;
 var connection = new signalR.HubConnectionBuilder().withUrl("/mmo").build();
-var keyboardStatus = { up: false, down: false, left: false, right: false };
+
+connection.start().then(function () {
+    document.getElementById("joinGameButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
 
 connection.on("gameUpdate", function (players) {
-    ctx.fillStyle = "rgb(0, 0, 0)";
-    ctx.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+    gameCanvas.context.fillStyle = "gray";
+    gameCanvas.context.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
     showPlayers(players);
 });
 
@@ -20,29 +22,24 @@ connection.on("playerCreated", function playerCreated(id) { playerId = id; });
 function showPlayers(players) {
     var playerSize = 25;
     var self;
-    var color = 'yellow';
+    var color = 'cyan';
     for (var i = 0; i < players.length; i++) {
         var player = players[i];
         if (player.id == playerId) { self = player; }
-        fillCircle(player.position.x, player.position.y, playerSize, color, player.name);
+        drawPlayer(player.position.x, player.position.y, playerSize, color, player.name);
     }
     if (self !== null) {
-        fillCircle(self.position.x, self.position.y, playerSize, 'red', self.name);
+        drawPlayer(self.position.x, self.position.y, playerSize, 'hotpink', self.name);
     }
 }
 
-connection.start().then(function () {
-    document.getElementById("joinGameButton").disabled = false;
-}).catch(function (err) {
-    return console.error(err.toString());
-});
 
 function startGameLoop() {
     setInterval(sendUpdates, 40);
 }
 
 function sendUpdates() {
-    move(getMovement());
+    move(gameCanvas.getMovement());
 }
 
 function joinGame() {
@@ -57,55 +54,23 @@ function joinGame() {
 
 function move(movement) {
     if (movement.x == 0 && movement.y == 0) { return;}
-    connection.invoke("Move", playerId, { x: movement.x, y: movement.y })
+    connection.invoke("Move", playerId, { x: movement.x * speed, y: movement.y * speed})
         .catch(function (err) {
             alert("Error sending move to server");
         });
 }
-function fillCircle(x, y, radius, color, name) {
-
-    ctx.fillStyle = color;
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.fillStyle = 'black';
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.font = '22px serif';
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    ctx.strokeStyle = '2px white';
-    //ctx.strokeText(name, x, y);
-    ctx.fillStyle = 'black';
-    ctx.fillText(name, x, y);
+function drawPlayer(x, y, radius, color, name) {
+    gameCanvas.context.fillStyle = color;
+    gameCanvas.context.beginPath();
+    gameCanvas.context.arc(x, y, radius, 0, 2 * Math.PI);
+    gameCanvas.context.fill();
+    gameCanvas.context.fillStyle = 'black';
+    gameCanvas.context.beginPath();
+    gameCanvas.context.arc(x, y, radius, 0, 2 * Math.PI);
+    gameCanvas.context.stroke();
+    gameCanvas.context.font = '22px serif';
+    gameCanvas.context.textAlign = 'center';
+    gameCanvas.context.textBaseline = 'middle';
+    gameCanvas.context.strokeStyle = '2px white';
+    gameCanvas.context.fillText(name, x, y);
 }
-
-function keyDown(event) {
-    switch (event.code) {
-        case 'ArrowLeft': keyboardStatus.left=true; break;
-        case 'ArrowRight': keyboardStatus.right = true;break;
-        case 'ArrowUp': keyboardStatus.up = true;break;
-        case 'ArrowDown': keyboardStatus.down= true;break;
-    }
-}
-
-function keyUp(event) {
-    switch (event.code) {
-        case 'ArrowLeft': keyboardStatus.left = false; break;
-        case 'ArrowRight': keyboardStatus.right = false; break;
-        case 'ArrowUp': keyboardStatus.up = false; break;
-        case 'ArrowDown': keyboardStatus.down = false; break;
-    }
-}
-
-function getMovement() {
-    var deltaX = 0;
-    deltaX += keyboardStatus.left ? -speed : 0;
-    deltaX += keyboardStatus.right ? speed : 0;
-    var deltaY = 0;
-    deltaY += keyboardStatus.up ? -speed : 0;
-    deltaY += keyboardStatus.down ? speed : 0;
-    return { x: deltaX, y: deltaY };
-}
-
